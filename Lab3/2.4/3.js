6b3bbad207b6472dca545198f258e062602e8118
kaliyev <157282519+spoonuser@users.noreply.github.com>
2025-02-09 11:51:43 +0400

    Add files via upload

---
+++ Lab3/2.4/3.js
@@ -0,0 +1,3 @@
+const BIRTHDAY = '18.04.1982'; // Uppercase isn`t mistake
+
+const AGE = someCode(BIRTHDAY); 
---
+++ Lab3/2.5/1.js
@@ -0,0 +1,7 @@
+let name = "Ilya";
+
+alert( `hello ${1}` ); // output: hello 1
+
+alert( `hello ${"name"}` ); // output: hello name
+
+alert( `hello ${name}` ); // ?output: hello Ilya
